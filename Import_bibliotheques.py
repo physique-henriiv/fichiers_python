e3d8ad33dd75575edea9dc0af186d38478630349
physique-henriiv <rouxlaurent3@orange.fr>
2026-02-19 14:08:47 +0100

    Simplification de l'installation des modules et passage à l'import .py pour JupyterLite

--- Import_bibliotheques.py
+++ Import_bibliotheques.py
@@ -10,58 +10,54 @@ import ipywidgets as widgets
 from IPython.display import display, clear_output
 import sys
 
-# Installation automatique pour JupyterLite
-if 'pyodide' in sys.modules:
-    try:
-        import piplite
-    except ImportError:
-        pass
+# Accès à l'espace de noms du notebook
+main = sys.modules['__main__']
 
 def tableurVersVariables(fichier, delimiter=','):
-    tableau = genfromtxt(fichier, delimiter=delimiter, skip_header=0, names=True)
+    """Importe un CSV et crée les variables directement dans le notebook."""
+    tableau = np.genfromtxt(fichier, delimiter=delimiter, skip_header=0, names=True)
     for i in tableau.dtype.names:
-        globals()[i] = tableau[i]
+        setattr(main, i, tableau[i])
 
 def Modele(expression, x, y, contraintes):
     modele = ExpressionModel(expression)
     parametres = modele.make_params()
-    for i in parametres :
-        modele.set_param_hint(i, value = 1)
-    for j in contraintes :
-        if j[0] in parametres :
-            modele.set_param_hint(j[0], value = j[1], vary = j[2], min = j[3], max = j[4])
+    for i in parametres:
+        modele.set_param_hint(i, value=1)
+    for j in contraintes:
+        if j[0] in parametres:
+            modele.set_param_hint(j[0], value=j[1], vary=j[2], min=j[3], max=j[4])
     parametres = modele.make_params()
-    resultat = modele.fit(y, parametres, x = x)
+    resultat = modele.fit(y, parametres, x=x)
     valeurs = ""
     for key in resultat.params:
-        # On garde votre f-string préférée
-        # On s'assure juste que stderr n'est pas None pour le formatage .2g
         if resultat.params[key].stderr is not None:
             valeurs += f"{key} = {resultat.params[key].value:.3g} ; incertitude : {resultat.params[key].stderr:.2g}\n"
         else:
             valeurs += f"{key} = {resultat.params[key].value:.3g} ; incertitude : ?\n"
-    return(modele, resultat.params, valeurs, expression)
+    return (modele, resultat.params, valeurs, expression)
 
 def Calcul_modele(abscisse_name, ordonnee_name, equation, debut, fin, debutCourbe, finCourbe, contraintes):
     ord_val = ordonnee_name
     eq_val = equation
-    equation_mod = re.sub(r"\b"+abscisse_name+r"\b","x", equation)
+    equation_mod = re.sub(r"\b" + abscisse_name + r"\b", "x", equation)
     
-    import __main__
-    abscisse = getattr(__main__, abscisse_name)
-    ordonnee = getattr(__main__, ordonnee_name)
+    # On récupère les variables du notebook
+    abscisse = getattr(main, abscisse_name)
+    ordonnee = getattr(main, ordonnee_name)
     
-    if debutCourbe == None :
+    if debutCourbe is None:
         debutCourbe = min(abscisse)
-    if finCourbe == None :
+    if finCourbe is None:
         finCourbe = max(abscisse)
     
-    xMod = linspace(debutCourbe, finCourbe, 30)
+    xMod = np.linspace(debutCourbe, finCourbe, 30)
     modele, parametres, valeurs, expression = Modele(equation_mod, abscisse[debut:fin], ordonnee[debut:fin], contraintes)
     expression = f"{ord_val} = {eq_val}"
-    yMod = modele.eval(parametres, x = xMod)
+    yMod = modele.eval(parametres, x=xMod)
     
+    # On injecte les paramètres trouvés (ex: a, b) comme variables globales dans le notebook
     for key in parametres:
-        setattr(__main__, key, parametres[key].value)
+        setattr(main, key, parametres[key].value)
     
-    return(xMod, yMod, expression, valeurs, abscisse, ordonnee, modele, parametres)
+    return (xMod, yMod, expression, valeurs, abscisse, ordonnee, modele, parametres)
